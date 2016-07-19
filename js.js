057b56ebe3ed9bc484d9b62ba02387edf9c73070
朱大威 <420219496@qq.com>
2016-07-19 22:39:20 +0800

    Update js.js

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
 var now = new Date();
-var date = new Date("2016-03-20 0:13:45");
+var date = new Date("03/20/2016 0:13:45");
 var h=(now.getTime()-date.getTime())/(24*60*60*1000);
 h=Math.round(h);
 document.getElementById('big700').innerHTML=h;
